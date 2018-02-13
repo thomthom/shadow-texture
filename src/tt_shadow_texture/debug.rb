c5c3602049ffa12abbcfb5b72c24264a41fdefbb
Thomas Thomassen <thomas@thomthom.net>
2018-02-13 18:37:15 +0100

    Adjust debug reload method.

--- src/tt_shadow_texture/debug.rb
+++ src/tt_shadow_texture/debug.rb
@@ -13,22 +13,12 @@ module TT::Plugins::ShadowTexture
   #   TT::Plugins::ShadowTexture.reload
   #
   # @return [Integer] Number of files reloaded.
-  # noinspection RubyGlobalVariableNamingConvention
   def self.reload
     original_verbose = $VERBOSE
     $VERBOSE = nil
-    # Core file (this)
     load __FILE__
-    # Supporting files
-    if defined?(PATH) && File.exist?(PATH)
-      x = Dir.glob(File.join(PATH, '**/*.{rb,rbs}')).each { |file|
-        # noinspection RubyResolve
-        load file
-      }
-      x.length + 1
-    else
-      1
-    end
+    pattern = File.join(__dir__, '**/*.rb')
+    Dir.glob(pattern).each { |file| load file }.size
   ensure
     $VERBOSE = original_verbose
   end
